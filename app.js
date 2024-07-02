46fccb7230195bfb0397880d9a132f4e5c93a307
Harin-Mabao <mixharinmabao@gmail.com>
2024-07-03 02:06:33 +0700

    Update app.js Rev00 First issued

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// write code here
+let a = prompt("พิมตัวเลข")
+if (a == 0 ) {
+    alert('Zero');
+} else if (a > 0 ){
+    alert('Positive number');}
+    else if (a < 0 ){
+        alert('Negative number');}
+    else if (isNaN(a)){
+        alert('Invalid number');}
